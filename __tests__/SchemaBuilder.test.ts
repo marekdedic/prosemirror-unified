29a09bd2d645a292641f4b9c316702c0a09f8907
Marek Dědič <developer@dedic.eu>
2023-05-22 14:34:01 +0200

    Added SchemaBuilder mark test

--- __tests__/SchemaBuilder.test.ts
+++ __tests__/SchemaBuilder.test.ts
@@ -1,7 +1,8 @@
-import { NodeType, Schema } from "prosemirror-model";
+import { MarkType, NodeType, Schema } from "prosemirror-model";
 
 import { SchemaBuilder } from "../src/SchemaBuilder";
 import { mockExtensionManager } from "./mockExtensionManager";
+import { mockMarkExtension } from "./mockMarkExtension";
 import { mockNodeExtension } from "./mockNodeExtension";
 
 test("SchemaBuilder works with nodes", () => {
@@ -22,6 +23,33 @@ test("SchemaBuilder works with nodes", () => {
   expect(schema).toBeInstanceOf(Schema);
   expect(schema.nodes["doc"]).toBeInstanceOf(NodeType);
   expect(schema.nodes["text"]).toBeInstanceOf(NodeType);
+  expect(schema.spec.nodes.get("doc")).toStrictEqual({});
+  expect(schema.spec.nodes.get("text")).toStrictEqual({});
+});
+
+test("SchemaBuilder works with marks", () => {
+  const docExtension = mockNodeExtension();
+  docExtension.proseMirrorNodeName.mockReturnValueOnce("doc");
+  docExtension.proseMirrorNodeSpec.mockReturnValueOnce({});
+  const textExtension = mockNodeExtension();
+  textExtension.proseMirrorNodeName.mockReturnValueOnce("text");
+  textExtension.proseMirrorNodeSpec.mockReturnValueOnce({});
+  const markExtension1 = mockMarkExtension();
+  markExtension1.proseMirrorMarkName.mockReturnValueOnce("MARK_1");
+  markExtension1.proseMirrorMarkSpec.mockReturnValueOnce({});
+
+  const manager = mockExtensionManager();
+  manager.markExtensions.mockReturnValueOnce([markExtension1]);
+  manager.nodeExtensions.mockReturnValueOnce([docExtension, textExtension]);
+
+  const builder = new SchemaBuilder(manager);
+  const schema = builder.build();
+
+  expect(schema).toBeInstanceOf(Schema);
+  expect(schema.marks["MARK_1"]).toBeInstanceOf(MarkType);
+  expect(schema.nodes["doc"]).toBeInstanceOf(NodeType);
+  expect(schema.nodes["text"]).toBeInstanceOf(NodeType);
+  expect(schema.spec.marks.get("MARK_1")).toStrictEqual({});
   expect(schema.spec.nodes.get("doc")).toStrictEqual({});
   expect(schema.spec.nodes.get("text")).toStrictEqual({});
 });
