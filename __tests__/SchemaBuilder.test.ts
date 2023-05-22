b2a998f5b7faa002fe03d13926a39bd15f39ed85
Marek Dědič <developer@dedic.eu>
2023-05-22 15:17:06 +0200

    Added SchemaBuilder complex spec test

--- __tests__/SchemaBuilder.test.ts
+++ __tests__/SchemaBuilder.test.ts
@@ -1,4 +1,10 @@
-import { MarkType, NodeType, Schema } from "prosemirror-model";
+import {
+  type DOMOutputSpec,
+  type Mark,
+  MarkType,
+  NodeType,
+  Schema,
+} from "prosemirror-model";
 
 import { SchemaBuilder } from "../src/SchemaBuilder";
 import { mockExtensionManager } from "./mockExtensionManager";
@@ -51,5 +57,62 @@ test("SchemaBuilder works with marks", () => {
   expect(schema.nodes["text"]).toBeInstanceOf(NodeType);
   expect(schema.spec.marks.get("MARK_1")).toStrictEqual({});
   expect(schema.spec.nodes.get("doc")).toStrictEqual({});
+  expect(schema.spec.nodes.get("text")).toStrictEqual({});
+});
+
+test("SchemaBuilder works with complex specs", () => {
+  const docExtension = mockNodeExtension();
+  docExtension.proseMirrorNodeName.mockReturnValueOnce("doc");
+  const docSpec = {
+    content: "text*",
+    group: "block",
+    code: true,
+    defining: true,
+    marks: "",
+    parseDOM: [{ tag: "pre" }],
+    toDOM(): DOMOutputSpec {
+      return ["pre", ["code", 0]];
+    },
+  };
+  docExtension.proseMirrorNodeSpec.mockReturnValueOnce(docSpec);
+  const textExtension = mockNodeExtension();
+  textExtension.proseMirrorNodeName.mockReturnValueOnce("text");
+  textExtension.proseMirrorNodeSpec.mockReturnValueOnce({});
+  const markExtension1 = mockMarkExtension();
+  markExtension1.proseMirrorMarkName.mockReturnValueOnce("MARK_1");
+  const markSpec = {
+    attrs: { href: { default: null } },
+    inclusive: false,
+    parseDOM: [
+      {
+        tag: "a[href]",
+        getAttrs(dom: Node | string): {
+          href: string | null;
+        } {
+          return {
+            href: (dom as HTMLElement).getAttribute("href"),
+          };
+        },
+      },
+    ],
+    toDOM(node: Mark): DOMOutputSpec {
+      return ["a", node.attrs];
+    },
+  };
+  markExtension1.proseMirrorMarkSpec.mockReturnValueOnce(markSpec);
+
+  const manager = mockExtensionManager();
+  manager.markExtensions.mockReturnValueOnce([markExtension1]);
+  manager.nodeExtensions.mockReturnValueOnce([docExtension, textExtension]);
+
+  const builder = new SchemaBuilder(manager);
+  const schema = builder.build();
+
+  expect(schema).toBeInstanceOf(Schema);
+  expect(schema.marks["MARK_1"]).toBeInstanceOf(MarkType);
+  expect(schema.nodes["doc"]).toBeInstanceOf(NodeType);
+  expect(schema.nodes["text"]).toBeInstanceOf(NodeType);
+  expect(schema.spec.marks.get("MARK_1")).toStrictEqual(markSpec);
+  expect(schema.spec.nodes.get("doc")).toStrictEqual(docSpec);
   expect(schema.spec.nodes.get("text")).toStrictEqual({});
 });
