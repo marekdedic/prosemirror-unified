05e1ff53b0f4aeb9ca9a5ffc2b794e4d6a09b803
Marek Dědič <developer@dedic.eu>
2023-05-22 12:04:38 +0200

    Fixed SchemaBuilder test

--- __tests__/SchemaBuilder.test.ts
+++ __tests__/SchemaBuilder.test.ts
@@ -1,18 +1,27 @@
+import { NodeType, Schema } from "prosemirror-model";
+
 import { SchemaBuilder } from "../src/SchemaBuilder";
 import { mockExtensionManager } from "./mockExtensionManager";
 import { mockNodeExtension } from "./mockNodeExtension";
 
-describe("SchemaBuilder works", () => {
-  const manager = mockExtensionManager();
+describe("SchemaBuilder works with nodes", () => {
   const docExtension = mockNodeExtension();
   docExtension.proseMirrorNodeName.mockReturnValueOnce("doc");
   docExtension.proseMirrorNodeSpec.mockReturnValueOnce({});
   const textExtension = mockNodeExtension();
   textExtension.proseMirrorNodeName.mockReturnValueOnce("text");
   textExtension.proseMirrorNodeSpec.mockReturnValueOnce({});
+
+  const manager = mockExtensionManager();
   manager.markExtensions.mockReturnValueOnce([]);
   manager.nodeExtensions.mockReturnValueOnce([docExtension, textExtension]);
-  const builder = new SchemaBuilder(manager);
 
-  expect(builder.build()).toEqual({});
+  const builder = new SchemaBuilder(manager);
+  const schema = builder.build();
+
+  expect(schema).toBeInstanceOf(Schema);
+  expect(schema.nodes["doc"]).toBeInstanceOf(NodeType);
+  expect(schema.nodes["text"]).toBeInstanceOf(NodeType);
+  expect(schema.spec.nodes.get("doc")).toEqual({});
+  expect(schema.spec.nodes.get("text")).toEqual({});
 });
