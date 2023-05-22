2423c71c4dd346f4ce36af8f958bf6927724eced
Marek Dědič <developer@dedic.eu>
2023-05-22 15:55:09 +0200

    Added transofrm to jest config

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   coverageDirectory: "coverage",
   coverageProvider: "babel",
   preset: "ts-jest/presets/default-esm",
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
   resetMocks: true,
   testMatch: ["<rootDir>/__tests__/**/*.test.ts"],
 };
