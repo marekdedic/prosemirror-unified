4cd35da6120916cf16e35542f723f3b0f1795992
Marek Dědič <developer@dedic.eu>
2023-06-14 12:23:21 +0200

    Added tests with child nodes

--- __tests__/ProseMirrorToUnistConverter.test.ts
+++ __tests__/ProseMirrorToUnistConverter.test.ts
@@ -28,6 +28,129 @@ test("Converts basic document", () => {
 
   jest.spyOn(console, "warn").mockImplementation();
   expect(converter.convert(rootProseMirrorNode)).toStrictEqual(rootUnistNode);
+  expect(docExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
+    rootProseMirrorNode,
+    []
+  );
+  expect(console.warn).not.toHaveBeenCalled();
+});
+
+test("Converts a document with children", () => {
+  const textExtension = mocked(new MockNodeExtension());
+  textExtension.proseMirrorNodeName.mockReturnValueOnce("text");
+  const textUnistNode = { type: "text", value: "Hello World!" };
+  textExtension.proseMirrorNodeToUnistNodes.mockReturnValueOnce([
+    textUnistNode,
+  ]);
+
+  const docExtension = mocked(new MockNodeExtension());
+  docExtension.proseMirrorNodeName.mockReturnValueOnce("doc");
+  const rootUnistNode = { type: "root", children: [textUnistNode] };
+  docExtension.proseMirrorNodeToUnistNodes.mockReturnValueOnce([rootUnistNode]);
+
+  const manager = mocked(new ExtensionManager([]));
+  manager.nodeExtensions.mockReturnValue([docExtension, textExtension]);
+
+  const converter = new ProseMirrorToUnistConverter(manager);
+
+  const schema = new Schema({
+    nodes: {
+      doc: {
+        content: "text*",
+      },
+      text: {},
+    },
+  });
+  const textProseMirrorNode = schema.text("Hello World!");
+  const rootProseMirrorNode = schema.nodes["doc"].createAndFill({}, [
+    textProseMirrorNode,
+  ])!;
+
+  jest.spyOn(console, "warn").mockImplementation();
+  expect(converter.convert(rootProseMirrorNode)).toStrictEqual(rootUnistNode);
+  expect(textExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
+    textProseMirrorNode,
+    []
+  );
+  expect(docExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
+    rootProseMirrorNode,
+    [textUnistNode]
+  );
+  expect(console.warn).not.toHaveBeenCalled();
+});
+
+test("Converts a document with children of multiple types", () => {
+  const typeOneExtension = mocked(new MockNodeExtension());
+  typeOneExtension.proseMirrorNodeName.mockReturnValue("typeOne");
+  const typeOneUnistNode = { type: "one" };
+  typeOneExtension.proseMirrorNodeToUnistNodes.mockReturnValueOnce([
+    typeOneUnistNode,
+  ]);
+
+  const typeTwoExtension = mocked(new MockNodeExtension());
+  typeTwoExtension.proseMirrorNodeName.mockReturnValue("typeTwo");
+  const typeTwoUnistNode = { type: "two" };
+  typeTwoExtension.proseMirrorNodeToUnistNodes.mockReturnValueOnce([
+    typeTwoUnistNode,
+  ]);
+
+  const typeThreeExtension = mocked(new MockNodeExtension());
+  typeThreeExtension.proseMirrorNodeName.mockReturnValue("typeThree");
+
+  const docExtension = mocked(new MockNodeExtension());
+  docExtension.proseMirrorNodeName.mockReturnValueOnce("doc");
+  const rootUnistNode = {
+    type: "root",
+    children: [typeOneUnistNode, typeTwoUnistNode],
+  };
+  docExtension.proseMirrorNodeToUnistNodes.mockReturnValueOnce([rootUnistNode]);
+
+  const manager = mocked(new ExtensionManager([]));
+  manager.nodeExtensions.mockReturnValue([
+    docExtension,
+    typeOneExtension,
+    typeTwoExtension,
+    typeThreeExtension,
+  ]);
+
+  const converter = new ProseMirrorToUnistConverter(manager);
+
+  const schema = new Schema({
+    nodes: {
+      doc: {
+        content: "groupOne*",
+      },
+      typeOne: {
+        group: "groupOne",
+      },
+      typeTwo: {
+        group: "groupOne",
+      },
+      text: {},
+    },
+  });
+  const typeOneProseMirrorNode = schema.nodes["typeOne"].createAndFill({}, [])!;
+  const typeTwoProseMirrorNode = schema.nodes["typeTwo"].createAndFill({}, [])!;
+  const rootProseMirrorNode = schema.nodes["doc"].createAndFill({}, [
+    typeOneProseMirrorNode,
+    typeTwoProseMirrorNode,
+  ])!;
+
+  jest.spyOn(console, "warn").mockImplementation();
+  expect(converter.convert(rootProseMirrorNode)).toStrictEqual(rootUnistNode);
+  expect(typeOneExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
+    typeOneProseMirrorNode,
+    []
+  );
+  expect(typeTwoExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
+    typeTwoProseMirrorNode,
+    []
+  );
+  expect(typeThreeExtension.proseMirrorNodeToUnistNodes).not.toHaveBeenCalled();
+  expect(docExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
+    rootProseMirrorNode,
+    [typeOneUnistNode, typeTwoUnistNode]
+  );
   expect(console.warn).not.toHaveBeenCalled();
 });
 
