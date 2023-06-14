e5d391c2149dbca6fda171ecd0fb81caf8784b37
Marek Dědič <developer@dedic.eu>
2023-06-14 15:25:56 +0200

    Added test with for conversion with marks

--- __tests__/ProseMirrorToUnistConverter.test.ts
+++ __tests__/ProseMirrorToUnistConverter.test.ts
@@ -3,6 +3,7 @@ import { Schema } from "prosemirror-model";
 
 import { ExtensionManager } from "../src/ExtensionManager";
 import { ProseMirrorToUnistConverter } from "../src/ProseMirrorToUnistConverter";
+import { MockMarkExtension } from "./MockMarkExtension";
 import { MockNodeExtension } from "./MockNodeExtension";
 
 jest.mock("../src/ExtensionManager");
@@ -153,6 +154,117 @@ test("Converts a document with children of multiple types", () => {
   expect(docExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
     rootProseMirrorNode,
     [typeOneUnistNode, typeTwoUnistNode]
+  );
+  expect(console.warn).not.toHaveBeenCalled();
+});
+
+test("Converts a document with marks", () => {
+  const textExtension = mocked(new MockNodeExtension());
+  textExtension.proseMirrorNodeName.mockReturnValueOnce("text");
+  const textUnistNode = { type: "text", value: "Hello World!" };
+  textExtension.proseMirrorNodeToUnistNodes.mockReturnValueOnce([
+    textUnistNode,
+  ]);
+
+  const markOneExtension = mocked(new MockMarkExtension());
+  markOneExtension.proseMirrorToUnistTest
+    .mockReturnValueOnce(false)
+    .mockReturnValueOnce(true);
+  const bothMarksUnistNode = {
+    type: "text",
+    value: "Hello World!",
+    markOne: true,
+    markTwo: true,
+  };
+  markOneExtension.processConvertedUnistNode.mockReturnValueOnce(
+    bothMarksUnistNode
+  );
+
+  const markTwoExtension = mocked(new MockMarkExtension());
+  markTwoExtension.proseMirrorToUnistTest
+    .mockReturnValueOnce(true)
+    .mockReturnValueOnce(false);
+  const markTwoUnistNode = {
+    type: "text",
+    value: "Hello World!",
+    markTwo: true,
+  };
+  markTwoExtension.processConvertedUnistNode.mockReturnValueOnce(
+    markTwoUnistNode
+  );
+
+  const docExtension = mocked(new MockNodeExtension());
+  docExtension.proseMirrorNodeName.mockReturnValueOnce("doc");
+  const rootUnistNode = {
+    type: "root",
+    children: [textUnistNode],
+  };
+  docExtension.proseMirrorNodeToUnistNodes.mockReturnValueOnce([rootUnistNode]);
+
+  const manager = mocked(new ExtensionManager([]));
+  manager.markExtensions.mockReturnValue([markOneExtension, markTwoExtension]);
+  manager.nodeExtensions.mockReturnValue([docExtension, textExtension]);
+
+  const converter = new ProseMirrorToUnistConverter(manager);
+
+  const schema = new Schema({
+    nodes: {
+      doc: {
+        content: "text*",
+      },
+      text: {},
+    },
+    marks: {
+      markOne: {},
+      markTwo: {},
+    },
+  });
+  const textProseMirrorNode = schema
+    .text("Hello World!")
+    .mark([schema.marks["markTwo"].create(), schema.marks["markOne"].create()]);
+  const rootProseMirrorNode = schema.nodes["doc"].createAndFill({}, [
+    textProseMirrorNode,
+  ])!;
+
+  jest.spyOn(console, "warn").mockImplementation();
+  expect(converter.convert(rootProseMirrorNode)).toStrictEqual(rootUnistNode);
+  expect(textExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
+    textProseMirrorNode,
+    []
+  );
+
+  expect(markOneExtension.proseMirrorToUnistTest.mock.calls).toHaveLength(2);
+  expect(markOneExtension.proseMirrorToUnistTest).toHaveBeenCalledWith(
+    textUnistNode,
+    textProseMirrorNode.marks[0]
+  );
+  expect(markOneExtension.proseMirrorToUnistTest).toHaveBeenCalledWith(
+    markTwoUnistNode,
+    textProseMirrorNode.marks[1]
+  );
+  expect(markOneExtension.processConvertedUnistNode).toHaveBeenCalledWith(
+    markTwoUnistNode,
+    textProseMirrorNode.marks[1]
+  );
+
+  expect(markTwoExtension.proseMirrorToUnistTest.mock.calls).toHaveLength(2);
+  expect(markTwoExtension.proseMirrorToUnistTest).toHaveBeenCalledWith(
+    textUnistNode,
+    textProseMirrorNode.marks[0]
+  );
+  // TODO: This is correct, but not optimal
+  expect(markTwoExtension.proseMirrorToUnistTest).toHaveBeenCalledWith(
+    bothMarksUnistNode,
+    textProseMirrorNode.marks[1]
+  );
+  expect(markTwoExtension.processConvertedUnistNode).toHaveBeenCalledWith(
+    textUnistNode,
+    textProseMirrorNode.marks[0]
+  );
+
+  expect(docExtension.proseMirrorNodeToUnistNodes).toHaveBeenCalledWith(
+    rootProseMirrorNode,
+    [bothMarksUnistNode]
   );
   expect(console.warn).not.toHaveBeenCalled();
 });
