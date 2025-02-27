f0c95d7820780a71df34e4c680cc1b562f5ccfb8
Marek Dědič <developer@dedic.eu>
2025-02-28 00:38:17 +0100

    Removed jest

--- __tests__/integration/LinkExtension.ts
+++
@@ -1,8 +0,0 @@
-import type { Node as UnistNode } from "unist";
-
-import type { UnistText } from "./TextExtension";
-
-export interface UnistLink extends UnistNode {
-  children: Array<UnistText>;
-  type: "link";
-}
