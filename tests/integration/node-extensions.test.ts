7409df8aa407e841944b7165b8f63fe69e9769d4
Marek Dědič <developer@dedic.eu>
2025-02-27 20:52:57 +0100

    Migrated node-extensions test to vitest

--- tests/integration/node-extensions.test.ts
+++ tests/integration/node-extensions.test.ts
@@ -1,13 +1,17 @@
-import { type Mocked, mocked } from "jest-mock";
-import { createEditor } from "jest-prosemirror";
+import type { Node as UnistNode } from "unist";
+
 import { type Processor, unified } from "unified";
+import { expect, type Mocked, test, vi } from "vitest";
+import { ProseMirrorTester } from "vitest-prosemirror";
 
 import { ProseMirrorUnified } from "../../src/ProseMirrorUnified";
 import { ParagraphExtension, paragraphSpec } from "./ParagraphExtension";
 import { RootExtension, rootSpec, type UnistRoot } from "./RootExtension";
 import { TextExtension, textSpec } from "./TextExtension";
 
-jest.mock("unified");
+vi.mock("unified");
+
+/* eslint-disable @typescript-eslint/no-empty-function, no-console -- Testing console output */
 
 test("Parsing a document with a paragraph", () => {
   expect.assertions(13);
@@ -29,12 +33,14 @@ test("Parsing a document with a paragraph", () => {
   };
 
   const unifiedMock = {
-    parse: jest.fn().mockReturnValueOnce(unistTree),
-    runSync: jest.fn().mockImplementation((root: UnistRoot) => root),
-    stringify: jest.fn().mockReturnValueOnce(source),
+    parse: vi.fn<(file: string) => UnistNode>().mockReturnValueOnce(unistTree),
+    runSync: vi
+      .fn<(node: UnistNode) => UnistNode>()
+      .mockImplementation((root) => root),
+    stringify: vi.fn<(tree: UnistNode) => string>().mockReturnValueOnce(source),
   } as unknown as Mocked<Processor>;
 
-  mocked(unified).mockReturnValueOnce(unifiedMock);
+  vi.mocked(unified).mockReturnValueOnce(unifiedMock);
 
   const pmu = new ProseMirrorUnified([
     new RootExtension(),
@@ -50,29 +56,29 @@ test("Parsing a document with a paragraph", () => {
       "doc"
     ].create({}, pmu.schema().nodes["paragraph"].createAndFill({}, pmu.schema().text("Hello World!")));
 
-  jest.spyOn(console, "warn").mockImplementation();
-  createEditor(proseMirrorRoot).callback((content) => {
-    expect(content.schema.spec.marks.size).toBe(0);
-    expect(content.schema.spec.nodes.size).toBe(3);
-    expect(content.schema.spec.nodes.get("doc")).toBe(rootSpec);
-    expect(content.schema.spec.nodes.get("paragraph")).toBe(paragraphSpec);
-    expect(content.schema.spec.nodes.get("text")).toBe(textSpec);
-    expect(content.doc).toEqualProsemirrorNode(proseMirrorTree);
-    expect(unifiedMock.parse).toHaveBeenCalledTimes(1);
-    expect(unifiedMock.parse).toHaveBeenCalledWith(source);
-    expect(unifiedMock.runSync).toHaveBeenCalledTimes(1);
+  vi.spyOn(console, "warn").mockImplementation(() => {});
+  const testEditor = new ProseMirrorTester(proseMirrorRoot);
 
-    expect(pmu.serialize(content.doc)).toBe(source);
+  expect(testEditor.schema.spec.marks.size).toBe(0);
+  expect(testEditor.schema.spec.nodes.size).toBe(3);
+  expect(testEditor.schema.spec.nodes.get("doc")).toBe(rootSpec);
+  expect(testEditor.schema.spec.nodes.get("paragraph")).toBe(paragraphSpec);
+  expect(testEditor.schema.spec.nodes.get("text")).toBe(textSpec);
+  expect(testEditor.doc).toEqualProseMirrorNode(proseMirrorTree);
+  expect(unifiedMock.parse).toHaveBeenCalledTimes(1);
+  expect(unifiedMock.parse).toHaveBeenCalledWith(source);
+  expect(unifiedMock.runSync).toHaveBeenCalledTimes(1);
 
-    expect(unifiedMock.stringify).toHaveBeenCalledTimes(1);
-    expect(unifiedMock.stringify).toHaveBeenCalledWith(unistTree);
-  });
+  expect(pmu.serialize(testEditor.doc)).toBe(source);
+
+  expect(unifiedMock.stringify).toHaveBeenCalledTimes(1);
+  expect(unifiedMock.stringify).toHaveBeenCalledWith(unistTree);
 
   expect(console.warn).not.toHaveBeenCalled();
 });
 
 test("Parsing a document with no root node", () => {
-  jest.spyOn(console, "warn").mockImplementation();
+  vi.spyOn(console, "warn").mockImplementation(() => {});
 
   expect(
     () =>
@@ -82,7 +88,7 @@ test("Parsing a document with no root node", () => {
 });
 
 test("Parsing a document with no text node", () => {
-  jest.spyOn(console, "warn").mockImplementation();
+  vi.spyOn(console, "warn").mockImplementation(() => {});
 
   expect(
     () =>
@@ -134,12 +140,16 @@ test("Parsing a document with no link node", () => {
   };
 
   const unifiedMock = {
-    parse: jest.fn().mockReturnValueOnce(sourceUnistTree),
-    runSync: jest.fn().mockImplementation((root: UnistRoot) => root),
-    stringify: jest.fn().mockReturnValueOnce(target),
+    parse: vi
+      .fn<(file: string) => UnistNode>()
+      .mockReturnValueOnce(sourceUnistTree),
+    runSync: vi
+      .fn<(node: UnistNode) => UnistNode>()
+      .mockImplementation((root) => root),
+    stringify: vi.fn<(tree: UnistNode) => string>().mockReturnValueOnce(target),
   } as unknown as Mocked<Processor>;
 
-  mocked(unified).mockReturnValueOnce(unifiedMock);
+  vi.mocked(unified).mockReturnValueOnce(unifiedMock);
 
   const pmu = new ProseMirrorUnified([
     new RootExtension(),
@@ -153,26 +163,28 @@ test("Parsing a document with no link node", () => {
       "doc"
     ].create({}, pmu.schema().nodes["paragraph"].createAndFill({}, pmu.schema().text("Hello !")));
 
-  jest.spyOn(console, "warn").mockImplementation();
+  vi.spyOn(console, "warn").mockImplementation(() => {});
   const proseMirrorRoot = pmu.parse(source);
-  createEditor(proseMirrorRoot).callback((content) => {
-    expect(content.schema.spec.marks.size).toBe(0);
-    expect(content.schema.spec.nodes.size).toBe(3);
-    expect(content.schema.spec.nodes.get("doc")).toBe(rootSpec);
-    expect(content.schema.spec.nodes.get("paragraph")).toBe(paragraphSpec);
-    expect(content.schema.spec.nodes.get("text")).toBe(textSpec);
-    expect(content.doc).toEqualProsemirrorNode(targetProseMirrorTree);
-    expect(unifiedMock.parse).toHaveBeenCalledTimes(1);
-    expect(unifiedMock.parse).toHaveBeenCalledWith(source);
-    expect(unifiedMock.runSync).toHaveBeenCalledTimes(1);
+  const testEditor = new ProseMirrorTester(proseMirrorRoot);
 
-    expect(pmu.serialize(content.doc)).toBe(target);
+  expect(testEditor.schema.spec.marks.size).toBe(0);
+  expect(testEditor.schema.spec.nodes.size).toBe(3);
+  expect(testEditor.schema.spec.nodes.get("doc")).toBe(rootSpec);
+  expect(testEditor.schema.spec.nodes.get("paragraph")).toBe(paragraphSpec);
+  expect(testEditor.schema.spec.nodes.get("text")).toBe(textSpec);
+  expect(testEditor.doc).toEqualProseMirrorNode(targetProseMirrorTree);
+  expect(unifiedMock.parse).toHaveBeenCalledTimes(1);
+  expect(unifiedMock.parse).toHaveBeenCalledWith(source);
+  expect(unifiedMock.runSync).toHaveBeenCalledTimes(1);
 
-    expect(unifiedMock.stringify).toHaveBeenCalledTimes(1);
-    expect(unifiedMock.stringify).toHaveBeenCalledWith(targetUnistTree);
-  });
+  expect(pmu.serialize(testEditor.doc)).toBe(target);
+
+  expect(unifiedMock.stringify).toHaveBeenCalledTimes(1);
+  expect(unifiedMock.stringify).toHaveBeenCalledWith(targetUnistTree);
 
   expect(console.warn).toHaveBeenCalledWith(
     'Couldn\'t find any way to convert unist node of type "link" to a ProseMirror node.',
   );
 });
+
+/* eslint-enable */
